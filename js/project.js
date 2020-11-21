029848a46e5c18c1e775ebbb27d85cc96ad88a56
ajaypra0518 <74779607+ajaypra0518@users.noreply.github.com>
2020-11-21 14:43:48 +0530

    Add files via upload

---
+++ js/project.js
@@ -0,0 +1,120 @@
+
+var x = document.getElementById('preloader');
+function funload() {
+    x.style.display = 'none';
+
+}
+
+
+function myFunction(content) {
+    var x = document.getElementById(content);
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".sticky-top");
+        var $link = $(".nav-link");
+        var $logo = $(".navbar-brand")
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $link.height());
+        $link.toggleClass('scrolled', $(this).scrollTop() > $link.height());
+        $logo.toggleClass('scrolled', $(this).scrollTop() > $link.height());
+    });
+});
+
+
+
+
+function myfunction() {
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+
+
+        document.getElementById("dummy").style.position = "fixed";
+        document.getElementById("dummy").style.backgroundColor = "white";
+        document.getElementById("dummy").style.zIndex = "999999999"
+        document.getElementById("dummy").style.width = "100%"
+    }
+    else {
+        document.getElementById("dummy").style.position = "static";
+        document.getElementById("dummy").style.backgroundColor = "rgb(33, 37, 41)";
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+jQuery(document).ready(function ($) {
+
+
+    if ($('.owl-clients').length) {
+        $('.owl-clients').owlCarousel({
+            loop: true,
+            nav: false,
+            dots: true,
+            items: 1,
+            margin: 30,
+            autoplay: false,
+            smartSpeed: 700,
+            autoplayTimeout: 6000,
+            responsive: {
+                0: {
+                    items: 1,
+                    margin: 0
+                },
+                460: {
+                    items: 1,
+                    margin: 0
+                },
+                576: {
+                    items: 3,
+                    margin: 20
+                },
+                992: {
+                    items: 5,
+                    margin: 30
+                }
+            }
+        });
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
